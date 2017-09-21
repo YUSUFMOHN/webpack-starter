66a383ff9fae725be4e01f1f4f9a29fa58b796d1
Anna Monus <azaleamollis@outlook.com>
2017-09-21 03:28:42 +0200

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const PreloadWebpackPlugin = require("preload-webpack-plugin");
+const webpack = require("webpack");
+
+module.exports = {
+  entry: "./src/script.js",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: "babel-loader",
+          options: { presets: ["env"] }
+        }
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin(),
+    new PreloadWebpackPlugin()
+  ]
+};
